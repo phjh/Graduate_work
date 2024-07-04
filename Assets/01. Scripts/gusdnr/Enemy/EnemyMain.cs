a6fad13bef1f3fa163664b8a7b386deff34e9339
gusdnr1231 <1231phw@gmail.com>
2024-07-04 10:14:00 +0900

    24_07_04
    Edit Stat Script
    Add EnemyMain

--- Assets/01. Scripts/gusdnr/Enemy/EnemyMain.cs
+++ Assets/01. Scripts/gusdnr/Enemy/EnemyMain.cs
@@ -2,36 +2,87 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyMain : MonoBehaviour, IDamageable
+public class EnemyMain : PoolableMono, IDamageable
 {
-	[SerializeField] private EnemyDataSO enemyData;
+	public string EnemyName;
+	[SerializeField] private StatusSO enemyData;
+
+	[HideInInspector] public Stat MaxHP;
+	[HideInInspector] public Stat Attack;
+	[HideInInspector] public Stat AttackSpeed;
+	[HideInInspector] public Stat MoveSpeed;
 
 	public bool isAlive = true;
-
-	private Rigidbody EnemyRB;
-
-	public float CuttentHp
+	
+	public float CurrentHp
 	{
 		get 
 		{
-			return CuttentHp;
+			return CurrentHp;
 		}
 
 		set
 		{
-			CuttentHp = value;
+			CurrentHp = value;
 		}
 	}
 
+	private Rigidbody EnemyRB;
+	private Collider EnemyCollider;
+
+
+	public override void ResetPoolableMono()
+	{
+		enemyData.SetUpDictionary();
+
+		SetEnemyStat();
+	}
+
+	public override void EnablePoolableMono()
+	{
+		isAlive = true;
+
+		CurrentHp = MaxHP.GetValue();
+	}
+
+	private void SetEnemyStat()
+	{
+		MaxHP = enemyData.StatDictionary["MaxHP"];
+		Attack = enemyData.StatDictionary["Attack"];
+		AttackSpeed = enemyData.StatDictionary["AttackSpeed"];
+		MoveSpeed = enemyData.StatDictionary["MoveSpeed"];
+	}
+
+	#region IDamageable Methods
+
 	public void TakeDamage(float dmg)
 	{
-		CuttentHp = Mathf.Clamp(CuttentHp - dmg, 0, enemyData.MaxHP);
-		if(CuttentHp <= 0) DieObject();
+		if(dmg < 0) IncreaseHP(dmg);
+		else if(dmg == 0) return;
+		else if(dmg >= 1) DecreaseHP(dmg);
+	}
+
+	private void IncreaseHP(float dmg)
+	{
+		//Start Heel Effect
+		CurrentHp = Mathf.Clamp(CurrentHp + dmg, 0, MaxHP.GetValue());
+
+		if (CurrentHp <= 0) DieObject();
+	}
+
+	private void DecreaseHP(float dmg)
+	{
+		//Start Hit Effect
+		CurrentHp = Mathf.Clamp(CurrentHp - dmg, 0, MaxHP.GetValue());
+
+		if (CurrentHp <= 0) DieObject();
 	}
 
 	public void DieObject()
 	{
 		isAlive = false;
 	}
+
+	#endregion
 
 }
