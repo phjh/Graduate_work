88b255880a298e644764f4de94175301c3e17af1
gusdnr1231 <1231phw@gmail.com>
2024-07-13 21:28:54 +0900

    24_07_13
    Make Player HP UI

--- Assets/01. Scripts/gusdnr/UI/HealthUI.cs
+++ Assets/01. Scripts/gusdnr/UI/HealthUI.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
@@ -7,13 +8,19 @@ using UnityEngine.UI;
 public class HealthUI : MonoBehaviour
 {
 	[Header("Player Condition UI Elements")]
-	[SerializeField] private Image PlayerHPBar;
-	[SerializeField] private TextMeshProUGUI HPText;
+	[SerializeField] private RectTransform PlayerHPBar;
+	[SerializeField] private TextMeshProUGUI CurrentHpText;
+	[SerializeField] private TextMeshProUGUI MaxHpText;
+	[SerializeField] private Image HpBarImage;
 
-	private float MaxHP;
-	private float HPFeelAmount = 0;
+	[Header("UI Values")]
+	[SerializeField] private float ValueChangeDuration =0.1f;
+	[SerializeField] private Color DefaultColor;
+	[SerializeField] private Color EnemergencyColor;
 
-	Managers mngs;
+	private Managers mngs;
+	private float HpFeelAmount = 0;
+
 	//Player player
 
 	private void Start()
@@ -23,8 +30,17 @@ public class HealthUI : MonoBehaviour
 		//player = mngs.PlayerManager.player;
 	}
 
-	private void ChangeHPText()
+	public void ChangeHPText(float currentHp, float maxHp)
 	{
+		CurrentHpText.text = maxHp.ToString();
+		HpFeelAmount = Mathf.Clamp(currentHp * (1f / maxHp), 0f, 1f);
+		if (HpFeelAmount < 0.3f) HpBarImage.color = EnemergencyColor;
+		else if(HpFeelAmount >= 0.3f) HpBarImage.color = DefaultColor;
+		PlayerHPBar.DOScaleX(HpFeelAmount, ValueChangeDuration).SetEase(Ease.OutQuart);
+	}
 
+	public void ChangedMaxHpValue(float maxHp)
+	{
+		MaxHpText.text = maxHp.ToString();
 	}
 }
