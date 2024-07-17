57989b07a8595137e75af2b3b26c1b03b6d736e5
gusdnr1231 <1231phw@gmail.com>
2024-07-17 14:34:38 +0900

    24_07_17
    Add OreBlock method
    - ExposeOre

--- Assets/01. Scripts/phjh/TIleSystem/Blocks/Oreblock.cs
+++ Assets/01. Scripts/phjh/TIleSystem/Blocks/Oreblock.cs
@@ -2,16 +2,39 @@ using UnityEngine;
 
 public class OreBlock : Blocks
 {
+	[System.Serializable]
+	public struct MeshPair
+	{
+		public Mesh Mesh;
+		public Material Material;
+	}
+	
 	[Header("Block Value")]
-	public int MaxTimesToBreak = 1;
+	public int MaxTimesToBreak = 7;
+	public int ExposeToBreak = 3;
 	public ItemDataSO OreData;
 
+	[Header("Mesh Values")]
+	[SerializeField] private MeshPair OrePair;
+	[SerializeField] private MeshPair BreakablePair;
+
+	private MeshFilter OreMeshFilter;
+	private MeshRenderer OreMeshRenderer;
+
 	private int TimesToBreak;
+	private bool alreadyExposed = false;
 
 	//이외 모션 및 여러가지 여기서 처리 예정
 
 	public override void SetBlock()
 	{
+		if (OreMeshFilter == null) TryGetComponent(out  OreMeshFilter);
+		if (OreMeshRenderer == null) TryGetComponent(out OreMeshRenderer);
+
+		SetMesh(BreakablePair);
+
+		alreadyExposed = false;
+
 		blockType = BlockType.OreBlock;
 		TimesToBreak = MaxTimesToBreak;
 	}
@@ -20,11 +43,23 @@ public class OreBlock : Blocks
 	{
 		TimesToBreak--;
 		MiningEffect();
+		if(alreadyExposed == false && TimesToBreak <= ExposeToBreak)
+		{
+			alreadyExposed = true;
+			SetMesh(OrePair);
+		}
+
 		if (TimesToBreak <= 0)
 		{
 			DropOre();
 			DeleteBlock();
 		}
+	}
+
+	private void SetMesh(MeshPair InitMesh)
+	{
+		OreMeshFilter.mesh = InitMesh.Mesh;
+		OreMeshRenderer.material = InitMesh.Material;
 	}
 
 	private void DropOre()
